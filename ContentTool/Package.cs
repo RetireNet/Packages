888bf695cb2ac1450ae0535980c7f5c4927c1d86
Mads Nyborg <nyborg.mads@gmail.com>
2018-09-07 10:34:07 +0200

    some refactor

---
+++ ContentTool/Package.cs
@@ -0,0 +1,9 @@
+﻿namespace ContentTool
+{
+    public class Package 
+    {
+        public string Id { get; set; }
+        public string Affected { get; set; }
+        public string Fix { get; set; }        
+    }
+}
