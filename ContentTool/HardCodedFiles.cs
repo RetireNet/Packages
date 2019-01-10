67570f90243e179c539db11949f147ea2010aff3
John Korsnes <johnkors@gmail.com>
2019-01-10 10:42:36 +0100

    Adds Januar 2019 vulns (#13)

--- ContentTool/HardCodedFiles.cs
+++ ContentTool/HardCodedFiles.cs
@@ -22,7 +22,7 @@ namespace ContentTool
             return jsonFile;
         }
 
-            public static JsonFile GetSept11JsonFile()
+        public static JsonFile GetSept11JsonFile()
         {
             var jsonFile = new JsonFile
             {
@@ -43,13 +43,44 @@ namespace ContentTool
             return jsonFile;
         }
 
+        public static JsonFile GetJanuar2019()
+        {
+            var jsonFile = new JsonFile
+            {
+                Link = "https://github.com/aspnet/Announcements/issues/334",
+                Description = "Microsoft Security Advisory CVE-2019-0564: ASP.NET Core Denial of Service Vulnerability"
+            };
+
+
+
+            var r2 = new Recommendation("Microsoft.AspNetCore.WebSockets")
+                .InsteadOf("2.1.0", "2.1.1").Prefer("2.1.7")
+                .InsteadOf("2.2.0").Prefer("2.2.1");
+
+            var r3 = new Recommendation("Microsoft.AspNetCore.Server.Kestrel.Core")
+                .InsteadOf(	"2.1.0", "2.1.1", "2.1.2", "2.1.3").Prefer("2.1.7");
+            
+            var r4 = new Recommendation("System.Net.WebSockets.WebSocketProtocol")
+                .InsteadOf(	"4.5.0", "4.5.1", "4.5.2").Prefer("4.5.3");
+
+            var r5 = SameVersionRecommendation("Microsoft.NETCore.App");
+            var r6 = SameVersionRecommendation("Microsoft.AspNetCore.App");
+            var r7 = SameVersionRecommendation("Microsoft.AspNetCore.All");
+
+            jsonFile.Packages.AddRange(r2.Packages);
+            jsonFile.Packages.AddRange(r3.Packages);
+            jsonFile.Packages.AddRange(r4.Packages);
+            jsonFile.Packages.AddRange(r5);
+            jsonFile.Packages.AddRange(r6);
+            jsonFile.Packages.AddRange(r7);
+            return jsonFile;
+        }
+
         private static IEnumerable<Package> SameVersionRecommendation(string packageId)
         {
             var r = new Recommendation(packageId)
-                .InsteadOf("4.0.0", "4.1.0", "4.1.1").Prefer("4.1.3")
-                .InsteadOf("4.3.0", "4.3.1").Prefer("4.3.3")
-                .InsteadOf("4.4.0", "4.4.1", "4.4.2").Prefer("4.4.4")
-                .InsteadOf("4.5.0", "4.5.1").Prefer("4.5.3");
+                .InsteadOf("2.1.0", "2.1.1", "2.1.2", "2.1.3", "2.1.4", "2.1.5", "2.1.6").Prefer("2.1.7")
+                .InsteadOf("2.2.0").Prefer("2.2.1");
             return r.Packages;
         }
 
